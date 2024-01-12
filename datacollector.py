c37d0d15486c81adfabd3d2db7f370a2a093aca3
Mati <msredzinski@icloud.com>
2024-01-12 23:32:39 +0100

    concept

---
+++ datacollector.py
@@ -0,0 +1,3 @@
+# Process will be looping after one click in x y position and reading will loop and understand from context what charachter is doing in v2
+# but in v1 just it will collection data such as:
+# 1.life 2. mana 3. capacity 4. hunger/food 5. enemies 6.stamina 
